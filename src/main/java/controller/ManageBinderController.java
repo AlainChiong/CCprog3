d190687c94dee88564550427f7d6c6ba700b8b3c
Yestin <fallenangel10999@gmail.com>
2025-07-30 15:57:36 +0800

    Forgot to save again ragh

--- src/main/java/controller/ManageBinderController.java
+++ src/main/java/controller/ManageBinderController.java
@@ -1,5 +1,10 @@
 package main.java.controller;
 
+import main.java.model.MainModel;
+import main.java.view.MainView;
+
 public class ManageBinderController {
-    
+    public ManageBinderController(MainModel mainModel, MainView mainView, MainController mainController) {
+        
+    }
 }
